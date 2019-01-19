5f1786ff4fd2a4e679ede30fd41b7db571151de6
Dang Xuan Truong <truonghusk17@gmail.com>
2019-01-19 20:19:22 +0700

    commit lan 3

--- buoi2/src/Main.java
+++ buoi2/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.printf("alo");
+        System.out.println("Nhap mot so tu ban phim: ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        Main y = new Main();
+        y.tinh_gt(a);
+        System.out.println("Giai thua la: " + y.tinh_gt(a));
+    }
+
+    public int tinh_gt(int a) {
+        int giai_thua = 1;
+        int i;
+        for (i = 1; i <= a; i++) {
+            giai_thua *= i;
+        }
+        return giai_thua;
     }
 }
